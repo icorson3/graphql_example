55f86c75be309b09531ad62a9d47ff0afc25a551
Ilana Corson <ilana@turing.io>
2018-05-24 08:47:54 -0600

    set up omniauth and write a dream driven feature test

--- spec/features/user_can_login_through_github_spec.rb
+++ spec/features/user_can_login_through_github_spec.rb
@@ -5,5 +5,23 @@ describe "as a user" do
     visit "/"
 
     expect(page).to have_content("Log In through Github")
+
+    click_on "Log In through Github"
+
+    expect(page).to have_content("icorson3")
+    expect(page).to have_content("Ilana Corson")
+    expect(page).to have_content("Denver, CO")
+
+    within(".popular-repos", match: :first) do
+      expect(page).to have_content("sass_workshop")
+    end
+
+    within(".contributions") do
+      expect(page).to have_content("586 contributions in the last year")
+    end
+
+    within(".contribution-activity") do
+      expect(page).to have_content("Created 16 commits in 5 repositories")
+    end
   end
 end
