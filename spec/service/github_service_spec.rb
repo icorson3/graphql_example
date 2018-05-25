368a5e6972fef58e98da705c82ce86f8c1c77d55
Ilana Corson <ilana@turing.io>
2018-05-24 18:03:58 -0600

    fix service test to expect a hash of data coming back

--- spec/service/github_service_spec.rb
+++ spec/service/github_service_spec.rb
@@ -1,17 +1,12 @@
 require 'rails_helper'
 
 describe GithubService do
-  attr_reader :service
-
-  before(:each) do
-    user = stub_omniauth
-    @service = GithubService.new(user)
-  end
 
   describe 'github service' do
     it 'makes api call to finds my profile information' do
-
-        
+      user = stub_omniauth
+      results = GithubService.gather_information(user)[:name]
+      expect(results).to eq("Ilana Corson")
     end
   end
 end
