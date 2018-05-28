5ed4e69952b6ada4948d5862f2445d7dfd966e30
Ilana Corson <ilana@turing.io>
2018-05-28 14:52:31 -0600

    hot fix for company showing

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -6,7 +6,7 @@ class Follower
     @name = data[:node][:name].empty? ? "No name provided" : data[:node][:name]
     @bio = data[:node][:bio] == "" ? "No bio provided" : data[:node][:bio]
     @location = data[:node][:location].nil? ? "No location provided" : data[:node][:location]
-    @company = data[:node][:company].empty? ? "No company provided" : data[:node][:company]
+    @company = data[:node][:company] == "" ? "No company provided" : data[:node][:company]
     @login = data[:node][:login]
     @avatar_url = data[:node][:avatarUrl]
   end
